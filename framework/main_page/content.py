fb43274d1817675220645d96f42e6fba1625d686
sakhaline <alexandra.ivva@gmail.com>
2024-01-21 19:29:31 +0200

    Implement functionality to interact with main page

---
+++ framework/main_page/content.py
@@ -0,0 +1,5 @@
+from appium.webdriver.common.appiumby import AppiumBy
+
+
+SIDEBAR_BUTTON = (AppiumBy.ID, "com.ajaxsystems:id/menuDrawer")
+ADD_HUB_BUTTON = (AppiumBy.ID, "com.ajaxsystems:id/hubAdd")
