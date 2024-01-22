74639809e9f13caa6cd920499c1da8baa73cb16d
sakhaline <alexandra.ivva@gmail.com>
2024-01-22 12:45:18 +0200

    Implement dynamically determination of the UDID

--- utils/android_utils.py
+++ utils/android_utils.py
@@ -1,3 +1,6 @@
+import subprocess
+
+
 def android_get_desired_capabilities():
     return {
         'autoGrantPermissions': True,
@@ -13,3 +16,19 @@ def android_get_desired_capabilities():
         'appPackage': 'com.ajaxsystems',
         'appActivity': 'com.ajaxsystems.ui.activity.LauncherActivity'
     }
+
+
+def get_device_udid():
+    try:
+        result = subprocess.run(['adb', 'devices'], capture_output=True, text=True, check=True)
+        output_lines = result.stdout.splitlines()
+
+        if len(output_lines) > 1:
+            udid = output_lines[1].split('\t')[0].strip()
+            return udid
+        else:
+            return None
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error running adb devices: {e}")
+        return None
