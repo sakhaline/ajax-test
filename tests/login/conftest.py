8d932fd72c6b33fc4e2e49c73aacc8d921d5df2d
sakhaline <alexandra.ivva@gmail.com>
2024-01-22 12:25:40 +0200

    Implement tests for login

--- tests/login/conftest.py
+++ tests/login/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 
-from framework.login_page import LoginPage
+from framework.login_page.login_page import LoginPage
+from appium.webdriver.common.appiumby import AppiumBy
+
+
+ADD_HUB_BUTTON = (AppiumBy.ID, "com.ajaxsystems:id/snackbar_text")
+SNACKBAR = (AppiumBy.ID, "com.ajaxsystems:id/hubAdd")
 
 
 @pytest.fixture(scope='function')
--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,2 +1,34 @@
-def test_user_login(user_login_fixture):
-    assert True
+import pytest
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.common.exceptions import NoSuchElementException
+
+
+@pytest.mark.parametrize(
+        "email,password",
+        [
+            pytest.param("qa.ajax.app.automation@gmail.com",
+                         "11111",
+                         id="login with invalid password"),
+            pytest.param("automation@gmail.com",
+                         "qa_automation_password",
+                         id="login with invalid email"),
+        ]
+)
+def test_failed_login(user_login_fixture, email, password, SNACKBAR):
+    user_login_fixture.login(email, password)
+    try:
+        element = user_login_fixture.find_element(*SNACKBAR)
+        assert element.is_displayed(), "Snackbar element is present on the page."
+    except NoSuchElementException:
+        pytest.fail("Snackbar element is not present on the page.")
+
+
+def test_successful_login(user_login_fixture, ADD_HUB_BUTTON):
+    email = "qa.ajax.app.automation@gmail.com"
+    password = "qa_automation_password"
+    user_login_fixture.login(email, password)
+    try:
+        element = user_login_fixture.find_element(*ADD_HUB_BUTTON)
+        assert element.is_displayed(), "AddHub element is present on the page."
+    except NoSuchElementException:
+        pytest.fail("AddHub element is not present on the page.")
