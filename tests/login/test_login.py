d1597b356e9aebf5b422c1c8b6204c7bb6778fb5
sakhaline <alexandra.ivva@gmail.com>
2024-01-22 13:15:05 +0200

    Implement logging for tests

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,5 +1,10 @@
 import pytest
+import logging
 from selenium.common.exceptions import NoSuchElementException
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 @pytest.mark.parametrize(
@@ -14,20 +19,32 @@ from selenium.common.exceptions import NoSuchElementException
         ]
 )
 def test_failed_login(user_login_fixture, email, password, SNACKBAR):
+    logging.info(f'Running test_failed_login with email:'
+                 '{email}, password: {password}')
+
     user_login_fixture.login(email, password)
     try:
         element = user_login_fixture.find_element(*SNACKBAR)
-        assert element.is_displayed(), 'Snackbar element is present on the page.'
+        assert element.is_displayed()
+        logging.info('Test failed as expected, Snackbar element is present.')
     except NoSuchElementException:
+        logging.error('Test failed.'
+                      'Snackbar element is not present on the page.')
         pytest.fail('Snackbar element is not present on the page.')
 
 
 def test_successful_login(user_login_fixture, ADD_HUB_BUTTON):
     email = 'qa.ajax.app.automation@gmail.com'
     password = 'qa_automation_password'
+    logging.info(f'Running test_successful_login with email: '
+                 '{email}, password: {password}')
+
     user_login_fixture.login(email, password)
     try:
         element = user_login_fixture.find_element(*ADD_HUB_BUTTON)
-        assert element.is_displayed(), 'AddHub element is present on the page.'
+        assert element.is_displayed()
+        logging.info('Test passed, AddHub element is present.')
     except NoSuchElementException:
+        logging.error('Test failed. '
+                      'AddHub element is not present on the page.')
         pytest.fail('AddHub element is not present on the page.')
--- tests/sidebar/test_sidebar.py
+++ tests/sidebar/test_sidebar.py
@@ -1,17 +1,26 @@
 import pytest
+import logging
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common.exceptions import NoSuchElementException
 
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
 def test_sidebar_button_displayed(user_login_fixture,
                                   sidebar_fixture):
+    logging.info('Running test_sidebar_button_displayed')
+
     user_login_fixture.login('qa.ajax.app.automation@gmail.com',
                              'qa_automation_password')
     try:
         element = sidebar_fixture.find_element(AppiumBy.ID,
                                                'com.ajaxsystems:id/menuDrawer')
         assert element.is_displayed(), 'Sidebar button element is present on the page.'
+        logging.info('Test passed, Sidebar button is displayed.')
     except NoSuchElementException:
+        logging.error('Test failed. Sidebar button element is not present on the page.')
         pytest.fail('Sidebar button element is not present on the page.')
 
 
@@ -34,10 +43,15 @@ def test_sidebar_button_displayed(user_login_fixture,
     ]
 )
 def test_sidebar_content(sidebar_fixture, key, element, name):
+    logging.info(f"Running test_sidebar_content with element: {name}")
+
     sidebar_fixture.open_sidebar()
     try:
         element = sidebar_fixture.find_element(key, element)
         assert element.is_displayed(), f'{name} button element is present on the page.'
+        logging.info(f'Test passed, {name} button is displayed.')
     except NoSuchElementException:
+        logging.error(f'Test failed. {name} button element is not present on the page.')
         pytest.fail(f'{name} button element is not present on the page.')
-    sidebar_fixture.driver.back()
+    finally:
+        sidebar_fixture.driver.back()
